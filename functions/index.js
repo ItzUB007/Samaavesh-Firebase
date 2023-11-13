ff8de111f179c21b43065437c198fe31e6089b07
Utkarsh Bareja <utkarshpant029@gmail.com>
2023-11-13 22:40:31 +0530

    13Nov2023

--- functions/index.js
+++ functions/index.js
@@ -10,4 +10,13 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get("/policy", (req, res) => {
+  res.render("policy");
+});
+
+app.get("/gallery", (req, res) => {
+  res.render("gallery");
+});
+
 exports.app = functions.https.onRequest(app);
